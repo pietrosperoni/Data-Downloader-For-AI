4cb98a0c199a2489b6ee5780a2e2e0f6f0d70837
Pietro Speroni di Fenizio <git@piespe.net>
2023-04-18 17:33:53 +0200

    first version, reads prints and stores the summary

---
+++ read_wikipedia_light.py
@@ -0,0 +1,54 @@
+import requests
+import argparse
+from ai_summarizer import summarize_text
+
+def fetch_wikipedia_page(title):
+    url = "https://en.wikipedia.org/w/api.php"
+    params = {
+        "action": "parse",
+        "page": title,
+        "format": "json",
+        "prop": "text",
+        "utf8": 1,
+        "formatversion": 2,
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+    return data["parse"]["text"]
+
+def save_to_markdown(title, text):
+    filename = f"{title}.md"
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
+    print(f"File saved as: {filename}")
+
+def save_to_file(title, text, file_extension="md"):
+    filename = f"{title}.{file_extension}"
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(text)
+    print(f"File saved as: {filename}")
+
+def main():
+    parser = argparse.ArgumentParser(description="Summarize a Wikipedia page with a given context")
+    parser.add_argument("title", nargs="?", help="The title of the Wikipedia page")
+    parser.add_argument("context", nargs="?", help="The context of interest for summarization")
+    args = parser.parse_args()
+
+    if args.title and args.context:
+        wikipedia_title = args.title
+        context = args.context
+    else:
+        wikipedia_title = input("Enter the title of the Wikipedia page: ")
+        context = input("Enter the context of interest (leave empty for no context filtering): ")
+
+    html_content = fetch_wikipedia_page(wikipedia_title)
+    summary=summarize_text(html_content, context,500)
+
+    print(summary)
+
+    save_to_file(f"{wikipedia_title}_summary", summary)
+    quit()
+
+if __name__ == "__main__":
+    main()
+
